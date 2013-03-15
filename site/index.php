fbbe4d9d2acd7b4b7e752608a57cb77fb461c9df
Alex Fuller <afuller101@gmail.com>
2013-03-15 14:53:30 +0000

    Site Files

---
+++ site/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link href="style.css" rel="stylesheet" type="text/css">
+	<title>Eva's Petite Bakery</title>
+</head>
+
+<body>
+	<div id="container">
+		<div id="logo">
+			<a href="#">
+				<img src="images/logo.png" width="140" height="140" alt="Logo">
+			</a>
+		</div>
+		<div id="cart"></div>
+		<nav>
+			<ul>
+				<li>Home</li>
+				<li>Cupcakes</li>
+				<li>Gourmet Sandwich</li>
+				<li>Corporate</li>
+				<li>Locations</li>
+				<li>Delivery</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+		<div id="left">hi</div>
+		<div id="middle">hi</div>
+		<div id="right">hi</div>
+		<div id="footer">Footer</div>
+	</div>
+</body>
+</html>
